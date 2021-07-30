b2d59c02a17759b6dce176754e203c7b743e851a
MohithKrish74 <87643124+MohithKrish74@users.noreply.github.com>
2021-07-30 14:02:57 +0530

    Add files via upload

---
+++ DefaultAsClass.java
@@ -0,0 +1,61 @@
+package com.keyword;
+
+class Animal         //Default Class
+{
+    void Color()
+    {
+        System.out.println("What is the Color ? ");
+    }
+    void Category()
+    {
+        System.out.println("What is the Category ?");
+    }
+    void Sound()
+    {
+        System.out.println("I Barks.");
+    }
+    void ChildName()
+    {
+        System.out.println("What is the Child Name ? ");
+    }
+    void Competitor()
+    {
+        System.out.println("What is the Competitor ? ");
+    }
+}
+class Dog extends Animal
+{
+    void Color()
+    {
+        System.out.println("I am Brown in Color. ");
+    }
+    void Category()
+    {
+        System.out.println("My Category is Pug.");
+    }
+    void Sound()
+   {
+       System.out.println("I Barks.");
+   }
+    void ChildName()
+    {
+        System.out.println("My Child Name is Puppy. ");
+    }
+    void Competitor()
+    {
+        System.out.println("My Competitor is Cat. ");
+    }
+}
+public class DefaultAsClass
+{
+    public static void main(String[] args)
+    {
+        Dog puppy = new Dog();
+        System.out.println("Dog Says: ");
+        puppy.Color();
+        puppy.Sound();
+        puppy.Category();
+        puppy.ChildName();
+        puppy.Competitor();
+    }
+}
